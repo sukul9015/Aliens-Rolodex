4f22829ded935349e7d424a6fbc184cc2486b8a9
Ashutosh Kumar <ashutosh.kmr9015@gmail.com>
2021-01-29 23:38:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+
+      aliens : [
+        {
+          name : 'Canon Bolt',
+          id : '01'
+        },
+        {
+          name : 'Spider Monkey',
+          id : '02'
+        },
+        {
+          name : 'Sting Fly',
+          id : '03'
+        },
+        {
+          name : 'Heat Blast',
+          id : '04'
+        }
+      ]
+
+    };
+  }
+
+  render(){
+    return(
+      <div className="App">
+
+        {
+          this.state.aliens.map(alien => (
+            <h1 key = {alien.id} >{alien.name}</h1>
+          ))
+        }
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
